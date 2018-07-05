ef7e18ca287d9f58380c547c5da770be8802c6de
veekrum thapa <veekrum.thapa@gmail.com>
2018-07-05 19:59:27 +0545

    changing the float into integer

--- memteller.sh
+++ memteller.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-#set -xe
+set -xe
 
  memTotal=`top -b -n 2 -d1 | grep "KiB Mem" | tail -n1 | awk -F , '{ print $1 }' | awk '{print $4}'`
  memUsed=`top -b -n 2 -d1 | grep "KiB Mem" | tail -n1 | awk -F , '{ print $3 }' | awk '{print $1}'`
 
  percent=$( bc <<< "scale=2; $memUsed/$memTotal * 100")
- #echo $percent
+ 
+ percent=$( bc <<< "scale=2; $memUsed/$memTotal * 100") ## using the bc to calculate in floating num 
+ num=$(printf "%.0f" $percent) ##  changing the float into integer
+ echo $num
 
-    if (( `bc <<< "$percent >= 60"`  )); then
-	echo "You are using more than 60% Memory. Take a break my friend  " | `/usr/games/xcowsay`
+    if (( `bc <<< "$num >= 60"`  )); then
+	echo "You are using $num"%" RAM Memory. Take a break my friend  " | `/usr/games/xcowsay`
     
-      elif (( `bc <<< "$percent >= 70"`  )); then
-	echo "You are using more than 70% Memory. Kill that Goddamn Zombie Processes my friend " | `/usr/games/xcowsay`
+      elif (( `bc <<< "$num >= 70"`  )); then
+	echo "You are using  $num"%" RAM Memory. Kill that Goddamn Zombie Processes my friend " | `/usr/games/xcowsay`
 
-      elif (( `bc <<< "$percent >= 80"`  )); then
+      elif (( `bc <<< "$num >= 80"`  )); then
         echo "You are Torturing your computer. Try restarting all the applications or you will miss your changes :(  " | `/usr/games/xcowsay`
     fi
