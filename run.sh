f11906333ade1742a95b69035f758bd1db61136f
veekrum thapa <veekrum.thapa@gmail.com>
2018-07-05 17:30:08 +0545

    correcting

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd ~/Downloads
 git clone git@github.com:veekrum/memteller.git
 
 cd memteller
-chmod 755 *.sh
+chmod 755 memteller.sh
 
 line="*/30 * * * * export DISPLAY=:0 && cd /home/$NAME/Downloads && ./memteller.sh > /tmp/cronerror.log 2>&1"
 (/usr/bin/crontab -u $NAME -l; echo "$line" ) | /usr/bin/crontab -u $NAME -
