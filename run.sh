9586f0f276abeae02924413f0d6478f1f5fad231
veekrum thapa <veekrum.thapa@gmail.com>
2018-07-05 17:34:31 +0545

    correcting

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 
 NAME=$USER
 
-cd memteller
-chmod 755 memteller.sh
 
 line="*/30 * * * * export DISPLAY=:0 && cd /home/$NAME/Downloads && ./memteller.sh > /tmp/cronerror.log 2>&1"
 (/usr/bin/crontab -u $NAME -l; echo "$line" ) | /usr/bin/crontab -u $NAME -
