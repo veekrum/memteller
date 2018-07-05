7e8e8623c72ac2f4b309554ca459e2e6faf91d38
veekrum thapa <veekrum.thapa@gmail.com>
2018-07-05 17:45:33 +0545

    correcting

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#set -xe
+set -xe
 
 NAME=$USER
-
 
 line="*/30 * * * * export DISPLAY=:0 && cd /home/$NAME/Downloads && ./memteller.sh > /tmp/cronerror.log 2>&1"
 (/usr/bin/crontab -u $NAME -l; echo "$line" ) | /usr/bin/crontab -u $NAME -
